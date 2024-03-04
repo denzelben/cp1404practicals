9a7e1d640b89cb440932669215269c7f5a00616b
denzel <denzelbenaya@gmail.com>
2024-03-04 13:50:25 +0800

    modified subject_reader.py

--- prac_04/subject_reader.py
+++ prac_04/subject_reader.py
@@ -2,7 +2,33 @@
 CP1404/CP5632 Practical
 Data file -> lists program
 """
-
+"""
+function main()
+    datas = function get_data()
+    function print_data(datas)
+    
+function get_data()
+    datas = empty list
+    input_file = open(subject_data.txt)
+    for line in input_file
+        print(line)
+        print(repr(line))
+        line = line.strip()
+        parts = line.split(',')
+        print(parts)
+        parts[2] = int(parts[2])
+        print(parts)
+        print("----------")
+        datas.append(parts)
+    input_file.close()
+    return datas
+    
+function print_data(datas)
+    for data in datas
+        print(f"{data[0]} is taught by {data[1]:12} and has {data[2]:3} students")
+        
+function main()
+"""
 FILENAME = "subject_data.txt"
 
 
