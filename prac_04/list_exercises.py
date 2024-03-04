57b75103a1fc814032ab1fd85b5ee95922394478
denzel <denzelbenaya@gmail.com>
2024-03-04 14:06:46 +0800

    update

--- prac_04/list_exercises.py
+++ prac_04/list_exercises.py
@@ -1,3 +1,15 @@
+"""
+numbers = empty list
+for i in range of 5
+    get number
+    numbers.append(number)
+print(f"The first number is {numbers[0]}")
+print(f"The last number is {numbers[-1]}")
+print(f"The smallest number is {min(numbers)}")
+print(f"The largest number is {max(numbers)}")
+print(f"The average of the numbers is {sum(numbers) / len(numbers)}")
+"""
+
 TOTAL_NUMBERS = 5
 numbers = []
 for i in range(TOTAL_NUMBERS):
